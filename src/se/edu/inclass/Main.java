8a0007afa45dfdbed3ce9e0a59d3e3997f0e354d
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-20 19:39:35 +0800

    Add functionality using lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -7,6 +7,8 @@ import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     private TaskNameComparator taskNameComparator;
@@ -16,13 +18,17 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        printData(tasksData);
+//        printData(tasksData);
         System.out.println();
-        System.out.println("Printing deadlines");
+        System.out.println("Printing deadlines before sorting");
         printDeadlines(tasksData);
-
+        System.out.println("Printing deadlines after sorting");
+        printDeadlinesUsingStream(tasksData);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        ArrayList<Task> filteredList = filteredTaskListUsingStreams(tasksData, "11");
+        System.out.println("Filtered list of tasks");
+        printData(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -47,5 +53,19 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().compareToIgnoreCase(b.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filteredTaskListUsingStreams(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredList;
     }
 }
