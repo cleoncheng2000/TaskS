d2b2657d7032ed5408da0e199fc15de34091e99c
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-20 19:23:31 +0800

    consume new methods

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -23,6 +23,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines using streams: " + countDeadlineUsingStream(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
